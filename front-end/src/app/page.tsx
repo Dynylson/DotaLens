3956a0a10461ff3a162b9b6d2aa4e880bf7eef9f
Dyn Júnior <dynylsonjr@gmail.com>
2024-09-08 14:33:44 -0300

    feat: verify if user already have steamId

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,14 +1,29 @@
+'use client';
+
 import { Button } from "@/components/ui/button"
 import { FaSteamSymbol } from "react-icons/fa";
+import { getCookie } from "cookies-next";
 
 export default function Home() {
+  async function handleSteamAuthentication() {
+    const steamId = getCookie('steamId');
+
+    if (steamId) {
+      window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL_FRONT}/teste`;
+
+      return;
+    }
+
+    window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL_BACK}/auth/steam`;
+  }
+
   return (
     <>
       <div className="h-screen flex justify-center items-center bg-[url('/dota.png')] bg-cover bg-center">
         <div className="h-[80vh]">
           <div className="flex flex-col items-center gap-12">
             <h1 className="text-5xl font-medium">Dota Lens</h1>
-            <Button className="flex gap-2 bg-zinc-50 text-zinc-950 hover:bg-zinc-950 hover:text-zinc-50"><FaSteamSymbol size={18} /> Entrar na Steam</Button>
+            <Button onClick={handleSteamAuthentication} className="flex gap-2 bg-zinc-50 text-zinc-950 hover:bg-zinc-950 hover:text-zinc-50"><FaSteamSymbol size={18} /> Entrar com a Steam</Button>
           </div>
         </div>
       </div>
